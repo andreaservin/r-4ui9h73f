f98535afc157c1983119481a80cd836536f40063
Andrea <andiserm@gmail.com>
2023-01-16 21:41:11 -0300

    Agrego solucion a test fallido

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      text: "",
+      repeat: (e) => {
+        this.setState( { text: e.target.value })
+      }
+    }
+  }
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" onChange={this.state.repeat} placeholder="Empieza a escribir algo" />
+        <p className="repeater">{this.state.text}</p>
       </div>
     );
   }
